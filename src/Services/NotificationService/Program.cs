51c7add0ed63ceaf9f287a70932111db0ee33d67
DruHustle <andrewgotora@yahoo.com>
2026-01-22 18:50:41 -0500

    Wire up EventBus and AnomalyDetectedIntegrationEventHandler in NotificationService

--- src/Services/NotificationService/Program.cs
+++ src/Services/NotificationService/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.Identity.Web;
 using SmartFactory.Services.NotificationService.Application.Interfaces;
 using SmartFactory.Services.NotificationService.Infrastructure.Services;
+using SmartFactory.BuildingBlocks.EventBus.Abstractions;
+using SmartFactory.BuildingBlocks.EventBus.Implementations;
+using SmartFactory.Services.NotificationService.Application.IntegrationEvents;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +20,18 @@ builder.Services.AddSwaggerGen();
 // Register Services
 builder.Services.AddScoped<INotificationService, NotificationService>();
 
+// Register Event Bus
+var connectionString = builder.Configuration["AzureServiceBusConnectionString"] ?? "Endpoint=sb://placeholder.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=placeholder";
+builder.Services.AddSingleton<IEventBus>(sp => new AzureServiceBus(connectionString));
+
+// Register Integration Event Handlers
+builder.Services.AddTransient<AnomalyDetectedIntegrationEventHandler>();
+
 var app = builder.Build();
+
+// Subscribe to events
+var eventBus = app.Services.GetRequiredService<IEventBus>();
+eventBus.Subscribe<AnomalyDetectedIntegrationEvent, AnomalyDetectedIntegrationEventHandler>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
